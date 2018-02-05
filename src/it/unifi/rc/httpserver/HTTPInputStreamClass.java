35e4e9f31196d52b727d29e53234f1ad4b9753aa
Gabriele <Gabriele@172.25.51.239>
2018-02-05 16:22:35 +0100

    modifiche a inputStreamClass

--- src/it/unifi/rc/httpserver/HTTPInputStreamClass.java
+++ src/it/unifi/rc/httpserver/HTTPInputStreamClass.java
@@ -2,6 +2,7 @@ package it.unifi.rc.httpserver;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Scanner;
 
 public class HTTPInputStreamClass extends HTTPInputStream{
 	private InputStream inputS;
@@ -17,20 +18,9 @@ public class HTTPInputStreamClass extends HTTPInputStream{
 
 	@Override
 	public HTTPRequest readHttpRequest() throws HTTPProtocolException {//legge una stringa dall'input stream e ne genera l'HTTPRequest
-		String message = "";
-		HTTPRequest cast = null;
-		try {
-			int i = 0;
-			while((i = inputS.read()) != -1) {
-				message = message + (char)i;
-			}
-			System.out.println(message);
-			//cast = new HTTPRequestClass (message);
-			
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return cast;
+		Scanner sc = new Scanner(inputS);
+		System.out.print(sc.next());
+		return new HTTPRequestClass(null, null, null, null, null);
 	}
 
 	@Override
--- src/it/unifi/rc/httpserver/main.java
+++ src/it/unifi/rc/httpserver/main.java
@@ -8,11 +8,11 @@ import java.io.OutputStream;
 public class main {
 
 	public static void main(String[] args) throws HTTPProtocolException {
-		String a = "HTTP/1.0 200 OK\r\nHost: Ciao\r\n\r\naaaaaaaaaaaaa.";
-		OutputStream asd = new ByteArrayOutputStream();
-		HTTPOutputStreamClass ddd = new HTTPOutputStreamClass(asd);
-		//HTTPReply aaa = new HTTPReplyClass(a);
-		//ddd.writeHttpReply(aaa);
+		String string="ciao";
+		byte [] aaa = string.getBytes();
+		InputStream ab = new ByteArrayInputStream(aaa);
+		HTTPInputStreamClass a = new HTTPInputStreamClass(ab);
+		a.readHttpRequest();
 	}
 
 }
