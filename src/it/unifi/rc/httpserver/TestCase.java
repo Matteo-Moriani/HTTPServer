84578dcd31289776a145904d7e196085f4731164
jMattMorians <jMattMorians@users.noreply.github.com>
2018-02-16 11:07:44 +0100

    creati file testcase e appunti

---
+++ src/it/unifi/rc/httpserver/TestCase.java
@@ -0,0 +1,14 @@
+package it.unifi.rc.httpserver;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestCase {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
